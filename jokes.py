2cafbfafa1c0ffe7ad34e2af3ef7f5d76d8631b2
Thomas Turek <thomasturek93@gmail.com>
2022-05-31 16:04:28 -0600

    Create jokes.py
    Initial code from IDLE to VS

---
+++ jokes.py
@@ -0,0 +1,23 @@
+print('What do you get when you cross a snowman with a vampire?')
+
+input()
+print('Frostbite!')
+"print('Frostbite!')"
+print()
+
+print('What do dentists call an astronaut\'s cavity?')
+
+input()
+print('A black hole!')
+"print('A black hole!')"
+print()
+
+print('Knock knock.')
+
+input()
+print('Interrupting cow.')
+"print('Interrupting cow.')"
+input()
+print('interrupting cow wh' end= '')
+"print('interrupting cow wh', end= '')
+print('-MOO!)
